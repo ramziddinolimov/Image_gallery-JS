65347506b9bd75ffdcb85e1e1396694e40353a13
Ramziddin Olimov <ramizmafiya@gmail.com>
2022-08-06 23:36:53 +0500

    app js continued caruswl

--- app.js
+++ app.js
@@ -9,17 +9,54 @@ console.log(galleryElement);
 window.onload = () => {  // 3
     for(let i = 0; i < galleryElement.length; i++) {
         let newIndex = i
+        let clickImgIndex;
         galleryElement[i].onclick = () => {
+            clickImgIndex = newIndex
             console.log(i);
             function preview(){  // 7
                 let selectedImgUrl = galleryElement[newIndex].querySelector("img").src
                 previewImgElement.src = selectedImgUrl
                 console.log(selectedImgUrl);
             }
+
+            const prevBtnElement = document.querySelector(".prev")
+            const nextBtnElement = document.querySelector(".next")
+
+            if(newIndex == 0) {
+                prevBtnElement.style.display = "none"
+            }
+
+            if(newIndex >= galleryElement.length - 1) {
+                nextBtnElement.style.display = "none"
+            }
+            prevBtnElement.addEventListener("click", event => {
+                newIndex--;
+                if(newIndex >= galleryElement.length - 1){
+                    preview()
+                    prevBtnElement.style.display = "none"
+                } else {
+                    preview()
+                    nextBtnElement.style.display = "block"
+                }
+            })
+
+            nextBtnElement.addEventListener("click", event => {
+                newIndex++;
+                if(newIndex == 0){
+                    preview()
+                    nextBtnElement.style.display = "none"
+                } else {
+                    preview()
+                    nextBtnElement.style.display = "block"
+                }
+            })
             preview()
             previewBoxElement.classList.add("show")  // 4  css da xam yozamiz
 
             closeIconElement.addEventListener("click", event => {  // 6
+                newIndex = clickImgIndex
+                prevBtnElement.style.display = "block"
+                nextBtnElement.style.display = "block"
                 previewBoxElement.classList.remove("show")
             })
         }
